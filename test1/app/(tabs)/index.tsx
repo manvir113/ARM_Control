253ef7b076ea04f1980fd804413692e8301774f7
manvir113 <manvir19912@gmail.com>
2024-11-04 22:16:15 -0800

    fixing errors in test1 and adding upload picture

--- test1/app/(tabs)/index.tsx
+++ test1/app/(tabs)/index.tsx
@@ -2,17 +2,32 @@ import { View, StyleSheet} from 'react-native';
 import {Image} from 'expo-image';
 import ImageViewer from "@/components/ImageViewer";
 import Button from '@/components/Button';
+import * as ImagePicker from 'expo-image-picker';
+import {useState} from 'react';
 
 const PlaceHolderImage=require("@/assets/images/background-image.png");
 
 export default function Index() {
+  const [selectedImage, setSelectedImage]=useState<string|undefined>(undefined);
+  const pickImageAsync=async()=>{
+    let result=await ImagePicker.launchImageLibraryAsync({
+      allowsEditing:true,
+      quality:1,
+    });
+    if (!result.cancelled){
+      setSelectedImage(result.assets[0].uri);
+    }
+    else{
+      alert('you did not select any image');
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.container}>
-        <ImageViewer imgSource={PlaceHolderImage}/>
+        <ImageViewer imgSource={PlaceHolderImage} selectedImage={selectedImage}/>
       </View>
       <View style ={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo"/>
+        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}/>
         <Button label="Use this photo"/>
       </View>
     </View>
--- test1/components/Button.tsx
+++ test1/components/Button.tsx
@@ -1,29 +1,31 @@
 import { StyleSheet, View, Pressable, Text } from 'react-native';
-import  FontAwesome  from '@expo/vector-icons/FontAwesome';
+/* @tutinfo Import FontAwesome. */import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 type Props = {
   label: string;
-  theme?:'primary';
+  theme?: 'primary';
+  onPress?:()=>void;
 };
 
-export default function Button({ label, theme }: Props) {
-  if (theme==='primary'){
+export default function Button({ label, theme, onPress }: Props) {
+  if (theme === 'primary') {
     return (
-        <View
-            style={{
-                styles.buttonContainer,
-                {borderWidth:4,borderColor:'#ffd33d',borderRadius:18},
-            }}>
-            <Pressable
-                style={{styles.button,{backgroundColor:'#fff'}}}
-                onPress={()=>alert('you pressed a button.')}>
-                <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon}/>
-                <Text style={{styles.buttonLabel,{color:'#25292e'}}}>{label}</Text>
-            </Pressable>
-        </View>
+      <View
+        style={[
+          styles.buttonContainer,
+          { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 },
+        ]}>
+        <Pressable
+          style={[styles.button, { backgroundColor: '#fff' }]}
+          onPress={onPress}>
+          <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
+          <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
+        </Pressable>
+      </View>
     );
   }
-    return (
+
+  return (
     <View style={styles.buttonContainer}>
       <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
         <Text style={styles.buttonLabel}>{label}</Text>
@@ -48,6 +50,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
+  },
+  buttonIcon: {
+    paddingRight: 8,
   },
   buttonLabel: {
     color: '#fff',
